2f45d9ea2b919cb3fb2fece761f29b38ee8018a3
DilankaSandeep <dilankacm@gmail.com>
2023-08-16 00:20:46 +0530

    ID genaration and Name Validation Completion

--- Smart Banking CLI Application - Assignment 1/SmartBankingApp.java
+++ Smart Banking CLI Application - Assignment 1/SmartBankingApp.java
@@ -8,7 +8,7 @@ public class SmartBankingApp {
         final String COLOR_BLUE_BOLD = "\033[34;1m";
         final String COLOR_RED_BOLD = "\033[31;1m";
         final String COLOR_GREEN_BOLD = "\033[33;1m";
-        final String COLOR_YELLO_BOLD="\033[32;1m";
+        final String COLOR_YELLO_BOLD = "\033[32;1m";
         final String RESET = "\033[0m";
 
         final String DASHBOARD = "Welcome to Smart Banking System🏦";
@@ -22,19 +22,21 @@ public class SmartBankingApp {
         final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
         final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
 
-        String[] customerIds = new String[0];
+        String[] accountNumbers = new String[0];
         String[] customerNames = new String[0];
         Double[] accountBalance = new Double[0];
 
         String screen = DASHBOARD;
+        int i = 1;
+
         loop1: do {
             final String APP_TITLE = String.format("%s%s%s",
                     COLOR_BLUE_BOLD, screen, RESET);
 
             System.out.println(CLEAR);
-            System.out.printf("\t%s+%s+%s\n",COLOR_YELLO_BOLD,"-".repeat(APP_TITLE.length()-11),RESET);
+            System.out.printf("\t%s+%s+%s\n", COLOR_YELLO_BOLD, "-".repeat(APP_TITLE.length() - 11), RESET);
             System.out.print("\t|" + APP_TITLE + "|\n");
-            System.out.printf("\t%s+%s+%s\n",COLOR_YELLO_BOLD,"-".repeat(APP_TITLE.length()-11),RESET);
+            System.out.printf("\t%s+%s+%s\n", COLOR_YELLO_BOLD, "-".repeat(APP_TITLE.length() - 11), RESET);
             switch (screen) {
                 case DASHBOARD:
                     System.out.printf("\t[1]. %s\n", CREATE_ACCOUNT);
@@ -76,8 +78,88 @@ public class SmartBankingApp {
                     }
                     break;
                 case CREATE_ACCOUNT:
-                
 
+                    String id = String.format("SDB-" + "%05d", i);
+                    System.out.printf("\tID: %s\n", id);
+                    i++;
+                    String name;
+                    boolean valid;
+                  
+                    do {
+                        valid = true;
+                        System.out.print("\tName: ");
+                        name = SCANNER.nextLine().strip();
+                        if (name.isBlank()) {
+                            System.out.printf(ERROR_MSG, "Name can't be empty");
+                            valid = false;
+                            continue;
+                        }
+                        for (int j = 0; j < name.length(); j++) {
+                            if (!(Character.isLetter(name.charAt(j)) ||
+                                    Character.isSpaceChar(name.charAt(j)))) {
+                                System.out.printf(ERROR_MSG, "Invalid name");
+                                valid = false;
+                                break;
+                            }
+                        }
+                    } while (!valid);
+                    String initialAccountBalance;
+                    loop2:
+                    do {
+                        valid = true;
+                        System.out.printf("\tInitial Deposit: ");
+                        initialAccountBalance = SCANNER.nextLine();
+                        for (int j = 0; j < initialAccountBalance.length() - 3; j++) {
+                            if (!Character.isDigit(initialAccountBalance.charAt(j))) {
+                                valid = false;
+                                System.out.printf(ERROR_MSG, "Enter a Valid Amount");
+                                continue loop2;
+                            }
+                        }
+                        if (!(initialAccountBalance.charAt((initialAccountBalance.length() - 3)) == '.' || Character
+                                .isDigit(initialAccountBalance.charAt(initialAccountBalance.length() - 3)))) {
+                            valid = false;
+                            System.out.printf(ERROR_MSG, "Enter a Valid Amount ");
+                            continue loop2;
+
+                        } else {
+                            for (int j = initialAccountBalance.length() - 2; j < initialAccountBalance.length(); j++) {
+                                if (!Character.isDigit(initialAccountBalance.charAt(j))) {
+                                    valid = false;
+                                    System.out.printf(ERROR_MSG, "Enter a Valid Amount");
+                                    continue loop2;
+                                }
+                            }
+
+                        }
+
+                    } while (!valid);
+                    double initialAccountBalanceDouble = Double.parseDouble(initialAccountBalance);
+                    System.out.printf("\tDeposited: "+"%s%,.2f", "Rs.", initialAccountBalanceDouble);
+
+                    String[] newAccountNumbers = new String[accountNumbers.length + 1];
+                    String[] newCustomerNames = new String[customerNames.length + 1];
+                    Double[] newAccountBalances = new Double[accountBalance.length + 1];
+                    for (int j = 0; j < accountNumbers.length; j++) {
+                        newAccountNumbers[j] = accountNumbers[j];
+                        newCustomerNames[j] = customerNames[j];
+                        newAccountBalances[j]= accountBalance[j];
+                    }
+                    newAccountNumbers[newAccountNumbers.length - 1] = id;
+                    newCustomerNames[newAccountNumbers.length - 1] = name;
+                    newAccountBalances[newAccountNumbers.length-1] =initialAccountBalanceDouble;
+                    accountNumbers = newAccountNumbers;
+                    customerNames = newCustomerNames;
+                    accountBalance=newAccountBalances;
+
+                    System.out.println();
+                    System.out.printf(SUCCESS_MSG,
+                            String.format("%s:%s has been Created successfully", id, name));
+                    System.out.print("\tDo you want to continue adding (Y/n)? ");
+                    if (SCANNER.nextLine().strip().toUpperCase().equals("Y"))
+                        continue;
+                    screen = DASHBOARD;
+                    break;
             }
 
         } while (true);
